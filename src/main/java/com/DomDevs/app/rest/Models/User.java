3c2e5535fdc1579281ce0773025e044e8503febc
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2022-12-27 14:44:47 +0000

    set up User model with getters and setters

---
+++ src/main/java/com/DomDevs/app/rest/Models/User.java
@@ -0,0 +1,40 @@
+package com.DomDevs.app.rest.Models;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
