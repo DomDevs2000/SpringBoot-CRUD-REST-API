51b6992ccfdd9a39560d245abe1989388b2670e1
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2022-12-27 14:34:44 +0000

    initial commit

---
+++ src/test/java/com/DomDevs/app/rest/SpringBootCrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DomDevs.app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
