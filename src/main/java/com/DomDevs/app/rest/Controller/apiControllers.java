f608802a897de8c4ccc8012c872073997f98faad
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2022-12-27 15:45:02 +0000

    Added GET and POST routes

--- src/main/java/com/DomDevs/app/rest/Controller/apiControllers.java
+++ src/main/java/com/DomDevs/app/rest/Controller/apiControllers.java
@@ -1,12 +1,31 @@
 package com.DomDevs.app.rest.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.DomDevs.app.rest.Models.User;
+import com.DomDevs.app.rest.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class apiControllers {
+    @Autowired
+    private UserRepo userRepo;
+
     @GetMapping("/")
     public String getPage() {
         return "Hello World";
     }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+
+    @PostMapping(value = "/users/create")
+    public String saveUser(@RequestBody User user) {
+        userRepo.save(user);
+        return "User saved..";
+    }
+
 }
