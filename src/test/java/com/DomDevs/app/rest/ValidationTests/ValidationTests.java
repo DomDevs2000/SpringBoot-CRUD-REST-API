e7f1cb60203272661012191fe389a559c35d1c02
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2022-12-30 14:15:31 +0000

    removed unused code

--- src/test/java/com/DomDevs/app/rest/ValidationTests/ValidationTests.java
+++ src/test/java/com/DomDevs/app/rest/ValidationTests/ValidationTests.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BindingResult;
 
 import java.util.Set;
@@ -23,9 +22,6 @@ import static org.mockito.Mockito.when;
 @AutoConfigureMockMvc
 @SpringBootTest(classes = SpringBootCrudRestApiApplication.class)
 public class ValidationTests {
-    @Autowired
-    private MockMvc mockMvc;
-
 
     @MockBean
     private UserRepo userRepo;
