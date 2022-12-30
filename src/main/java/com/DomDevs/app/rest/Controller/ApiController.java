6ae5560616ff9ac59d7ac55c8b55729b49fb0710
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2022-12-30 19:54:34 +0000

    Implemented logging to Controller

--- src/main/java/com/DomDevs/app/rest/Controller/ApiController.java
+++ src/main/java/com/DomDevs/app/rest/Controller/ApiController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 
 @RestController
@@ -19,6 +20,8 @@ public class ApiController {
     @Autowired
     private UserRepo userRepo;
 
+    private final Logger LOGGER = Logger.getLogger(String.valueOf(ApiController.class));
+
     @GetMapping("/")
     public String getPage() {
         return "Welcome to my CRUD REST API project using Spring Boot, JPA, Maven and MYSQL. " + "Please use Postman/Insomnia to create POST/PUT/DELETE requests. " + "To get all users, please submit a GET request to '/users' " + "To create a new user, please submit a POST request to '/users/create' and fill in the JSON body with firstName, lastName and age. " + "To update a user, please submit a PUT request to '/users/update/{id}' and fill in the JSON body with the content you wish to update " + "To delete a user, please submit a DELETE request to 'users/delete/{id}' ";
@@ -26,19 +29,35 @@ public class ApiController {
 
     @GetMapping("/users")
     public List<User> getUsers() {
-        return userRepo.findAll();
+        try {
+            LOGGER.info("Successful GET Request");
+            return userRepo.findAll();
+        } catch (RuntimeException e) {
+            LOGGER.info("Failed to GET /users");
+            throw new RuntimeException();
+        }
+
+
     }
 
     @GetMapping(value = "/users/page={page}/size={pageSize}")
     public List<User> getPaginatedUsers(@PathVariable int page, @PathVariable int pageSize) {
-        return userService.findAllPaginated(page, pageSize);
+        try {
+            LOGGER.info("Successful GET Request");
+            return userService.findAllPaginated(page, pageSize);
+        } catch (RuntimeException e) {
+            LOGGER.info("Failed to GET Paginated Users");
+            throw new RuntimeException();
+        }
     }
 
     @PostMapping(value = "/users/create")
     public String createNewUser(@RequestBody @Valid User user, BindingResult errors) {
         if (errors.hasErrors()) {
+            LOGGER.info("Error:" + errors);
             throw new ValidationException(errors);
         } else {
+            LOGGER.info("Successful POST Request");
             userRepo.save(user);
             return "User Created";
         }
@@ -47,18 +66,22 @@ public class ApiController {
     @GetMapping(value = "/users/id/{id}")
     public User getOneUser(@PathVariable long id) {
         try {
+            LOGGER.info("Successful GET Request");
             return userRepo.findById(id).get();
         } catch (RuntimeException exception) {
+            LOGGER.info("Error Finding User");
             throw new UserNotFoundException(id);
         }
     }
 
     @GetMapping("/users/age/{age}")
-    public List<User> getAllByAge(@Valid @PathVariable  int age) {
+    public List<User> getAllByAge(@Valid @PathVariable int age) {
         try {
+            LOGGER.info("Successful GET Request");
             return userService.findAllByAge(age);
         } catch (RuntimeException e) {
-            System.err.println(e.getStackTrace());
+            e.getStackTrace();
+            LOGGER.info("Error Finding User");
             throw new AgeNotFoundException(age);
         }
     }
@@ -67,9 +90,11 @@ public class ApiController {
     @GetMapping("/users/firstname/{firstName}")
     public List<User> getAllByFirstName(@PathVariable String firstName) {
         try {
+            LOGGER.info("Successful GET Request");
             return userService.findAllByFirstName(firstName);
         } catch (RuntimeException exception) {
             exception.getStackTrace();
+            LOGGER.info("Error Finding User");
             throw new FirstNameNotFoundException(firstName);
         }
     }
@@ -77,9 +102,11 @@ public class ApiController {
     @GetMapping("/users/lastname/{lastName}")
     public List<User> getAllByLastname(@PathVariable String lastName) {
         try {
+            LOGGER.info("Success GET Request");
             return userService.findAllByLastName(lastName);
         } catch (RuntimeException exception) {
             exception.getStackTrace();
+            LOGGER.info("Error Finding User");
             throw new LastNameNotFoundException(lastName);
         }
     }
@@ -95,8 +122,11 @@ public class ApiController {
                 updatedUser.setLastName(user.getLastName());
                 updatedUser.setAge(user.getAge());
                 userRepo.save(updatedUser);
+                LOGGER.info("Successful PUT Request");
                 return "Updated User With The id: " + id;
             } catch (Exception e) {
+                e.printStackTrace();
+                LOGGER.info("Error Finding User");
                 throw new UserNotFoundException(id);
             }
         }
@@ -105,11 +135,14 @@ public class ApiController {
     @DeleteMapping(value = "users/delete/{id}")
     public String deleteUser(@PathVariable @Valid long id) {
         try {
+
             User deletedUser = userRepo.findById(id).get();
             userRepo.delete(deletedUser);
+            LOGGER.info("Successful DELETE Request");
             return "Deleted User With The id: " + id;
         } catch (RuntimeException exception) {
             exception.getStackTrace();
+            LOGGER.info("Error Finding User");
             throw new UserNotFoundException(id);
         }
     }
