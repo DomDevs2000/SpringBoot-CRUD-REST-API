5d358d792e3334445f38094fa448246ff3ee6214
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-03-16 20:19:45 +0000

    updated template file, removed comments out of lambda handler

--- src/main/java/com/DomDevs/app/rest/StreamLambdaHandler.java
+++ src/main/java/com/DomDevs/app/rest/StreamLambdaHandler.java
@@ -17,10 +17,7 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     static {
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(SpringBootCrudRestApiApplication.class);
-            // If you are using HTTP APIs with the version 2.0 of the proxy model, use the getHttpApiV2ProxyHandler
-            // method: handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Application.class);
         } catch (ContainerInitializationException e) {
-            // if we fail here. We re-throw the exception to force another cold start
             e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
